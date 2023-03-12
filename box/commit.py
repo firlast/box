5c1479fc6f913a55c36c7891b37b6ac3d2487561
Jaedson Silva <contacanal798@gmail.com>
2023-03-12 17:01:05 -0300

    feat: add _create_object() method to store file difference

--- box/commit.py
+++ box/commit.py
@@ -1,13 +1,25 @@
+import os
 import json
+import secrets
 
 
 class Commit:
-    def __init__(self, commit_filepath: str) -> None:
+    def __init__(self, commit_filepath: str, obj_filepath: str) -> None:
         self._commit_file = commit_filepath
+        self._obj_file = obj_filepath
 
     def _dump_commit_file(self, data: dict) -> None:
         with open(self._commit_file, 'w') as file:
             json.dump(data, file, indent=2)
+
+    def _create_object(self, file_diff: dict) -> str:
+        object_id = secrets.token_hex(32)
+        object_path = os.path.join(self._obj_file, object_id)
+
+        with open(object_path, 'w') as _object:
+            json.dump(file_diff, _object, separators=(',', ':'))
+
+        return object_id
 
     def get_commits(self, until_commit_id: str = None) -> dict:
         with open(self._commit_file, 'rb') as file:
