7727a408322f804666c5dac09711c0a983c6e382
Jaedson Silva <contacanal798@gmail.com>
2023-03-12 16:32:55 -0300

    feat: add _dump_commit_file() to save commits data

--- box/commit.py
+++ box/commit.py
@@ -1,3 +1,10 @@
+import json
+
+
 class Commit:
     def __init__(self, commit_filepath: str) -> None:
         self._commit_file = commit_filepath
+
+    def _dump_commit_file(self, data: dict) -> None:
+        with open(self._commit_file, 'w') as file:
+            json.dump(data, file, indent=2)
