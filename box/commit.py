676692d1ddd42b33f3348e612c23ffd13be2f93d
Jaedson Silva <contacanal798@gmail.com>
2023-03-12 16:39:34 -0300

    feat: add method to get commits

--- box/commit.py
+++ box/commit.py
@@ -8,3 +8,20 @@ class Commit:
     def _dump_commit_file(self, data: dict) -> None:
         with open(self._commit_file, 'w') as file:
             json.dump(data, file, indent=2)
+
+    def get_commits(self, until_commit_id: str = None) -> dict:
+        with open(self._commit_file, 'rb') as file:
+            commits = json.load(file)
+
+        if until_commit_id:
+            filtered_commits = {}
+
+            for cid, cdata in commits:
+                if cid != until_commit_id:
+                    filtered_commits[cid] = cdata
+                else:
+                    break
+
+            return filtered_commits
+
+        return commits
