94363c5b56b375c9d7f9ee9e8a904472fbcaf6ea
Jaedson Silva <contacanal798@gmail.com>
2023-03-12 15:53:58 -0300

    feat: dump updated tracker info

--- box/tracker.py
+++ box/tracker.py
@@ -28,11 +28,13 @@ class Tracker:
 
         return tracked
 
-    def update_committed_status(self, filepath: str, committed: bool) -> None:
+    def update_track_info(self, filepath: str, _hash: str, committed: bool) -> None:
         tracked = self.get_tracked()
 
         if filepath in tracked:
+            tracked[filepath]['hash'] = _hash
             tracked[filepath]['committed'] = committed
+            self._dump_tracker(tracked)
         else:
             raise exceptions.FileNotTrackedError(f'File "{filepath}" not tracked')
 
