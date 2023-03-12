85aa5c6cca1c2850603af038bb480bbb9fd014f1
Jaedson Silva <contacanal798@gmail.com>
2023-03-12 19:08:53 -0300

    feat: create commit() method to make first commit

--- box/commit.py
+++ box/commit.py
@@ -1,12 +1,20 @@
 import os
 import json
 import secrets
+import hashlib
+from typing import List
+from datetime import datetime
+
+from .tracker import Tracker
+from . import exceptions
+from . import utils
 
 
 class Commit:
     def __init__(self, repo_path: str) -> None:
         self._commit_file = os.path.join(repo_path, 'commits.json')
         self._obj_file = os.path.join(repo_path, 'objects')
+        self._tracker = Tracker(repo_path)
 
     def _dump_commit_file(self, data: dict) -> None:
         with open(self._commit_file, 'w') as file:
@@ -40,3 +48,39 @@ class Commit:
             return filtered_commits
 
         return commits
+
+    def commit(self, files: List[str], message: str) -> str:
+        tracked = self._tracker.get_tracked()
+        commits = self.get_commits()
+
+        # check if all files are tracked
+        for file in files:
+            if file not in tracked:
+                raise exceptions.FileNotTrackedError(f'File "{file}" not tracked')
+
+        commit_objects = {}
+
+        for file in files:
+            if not tracked[file]['committed']:
+                with open(file, 'r') as file_r:
+                    file_enum_lines = utils.enumerate_lines(file_r.readlines())
+
+                obj_id = self._create_object(file_enum_lines)
+                commit_objects[file] = obj_id
+
+        commit_datetime = str(datetime.now().replace(microsecond=0))
+        id_parts = ''.join((
+            secrets.token_hex(16),
+            message,
+            commit_datetime
+        ))
+
+        commit_id = hashlib.sha1(id_parts.encode()).hexdigest()
+
+        commits[commit_id] = dict(
+            message=message,
+            date=str(commit_datetime),
+            objects=object,
+        )
+
+        self._dump_commit_file(commits)
