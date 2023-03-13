5eaaba338ba17370daf3e0b8ada3817a17cdd01b
Jaedson Silva <contacanal798@gmail.com>
2023-03-13 19:18:37 -0300

    feat: add init() function to start a new repository with "init" argument

--- box/__main__.py
+++ box/__main__.py
@@ -1,5 +1,21 @@
+import os
+import sys
+
 from argeasy import ArgEasy
 from .__init__ import __version__
+
+REPO_PATH = os.path.join('./', '.box')
+OBJECTS_PATH = os.path.join(REPO_PATH, 'objects')
+
+
+def init() -> None:
+    if os.path.isdir(REPO_PATH):
+        print(f'\033[33mRepository already started in {repr(REPO_PATH)}\033[m')
+        sys.exit(1)
+    else:
+        os.mkdir(REPO_PATH)
+        os.mkdir(OBJECTS_PATH)
+        print(f'\033[32mNew repository started in {repr(REPO_PATH)}\033[m')
 
 
 def main() -> None:
@@ -10,3 +26,7 @@ def main() -> None:
     )
 
     parser.add_argument('init', 'Init a empty repository', action='store_true')
+    args = parser.parse()
+
+    if args.init:
+        init()
