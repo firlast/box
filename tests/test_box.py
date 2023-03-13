297315195805bf80d0b78c945a347555a3262b89
Jaedson Silva <contacanal798@gmail.com>
2023-03-13 18:24:55 -0300

    test: create test_commit_changed_file() test

--- tests/test_box.py
+++ tests/test_box.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+import json
 
 import bupytest
 
@@ -124,12 +125,47 @@ class TestCommit(bupytest.UnitTest):
         self.assert_expected(len(commits[commit_id]['objects']), 2, message='Expected 2 objects references')
 
         file_1_object_path = os.path.join('./.box/objects', commits[commit_id]['objects'][TEST_FILE_1])
-        file_2_object_path = os.path.join('./.box/objects', commits[commit_id]['objects'][TEST_FILE_1])
+        file_2_object_path = os.path.join('./.box/objects', commits[commit_id]['objects'][TEST_FILE_2])
 
-        self.assert_true(os.path.isfile(file_1_object_path),
-                         message=f'Object to {repr(TEST_FILE_1)} not created')
-        self.assert_true(os.path.isfile(file_2_object_path),
-                         message=f'Object to {repr(TEST_FILE_2)} not created')
+        self.assert_true(
+            value=os.path.isfile(file_1_object_path),
+            message=f'Object to {repr(TEST_FILE_1)} not created'
+        )
+
+        self.assert_true(
+            value=os.path.isfile(file_2_object_path),
+            message=f'Object to {repr(TEST_FILE_2)} not created'
+        )
+
+    def test_commit_changed_file(self):
+        with open(TEST_FILE_1, 'w') as file_1:
+            file_1.write(TEST_FILE_1_CONTENT_CHANGED)
+
+        commit_id = _commit.commit([TEST_FILE_1], message='second commit')
+        commits = _commit.get_commits()
+
+        self.assert_expected(len(commits), 2, message='Incorrect commits number')
+        self.assert_true(commits.get(commit_id), message='Commit ID not found')
+        self.assert_true(commits[commit_id]['message'], 'second commit')
+        self.assert_expected(len(commits[commit_id]['objects']), 1, message='Expected 1 object reference')
+
+        file_1_object_path = os.path.join('./.box/objects', commits[commit_id]['objects'][TEST_FILE_1])
+
+        self.assert_true(
+            value=os.path.isfile(file_1_object_path),
+            message=f'Object to {repr(TEST_FILE_1)} not created'
+        )
+
+        with open(file_1_object_path, 'r') as object_file:
+            object_lines = json.load(object_file)
+
+        self.assert_expected(
+            value=object_lines,
+            expected={
+                '1': 'This is a test!\n',
+                '2': 'A big test!'
+            }
+        )
 
 
 if __name__ == '__main__':
