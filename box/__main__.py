2dc31140bbb760d8754a29af2f7780f0a56921ce
Jaedson Silva <contacanal798@gmail.com>
2023-03-15 20:05:53 -0300

    feat: add "status" argument to view uncommitted and untracked files

--- box/__main__.py
+++ box/__main__.py
@@ -6,6 +6,7 @@ from argeasy import ArgEasy
 from .__init__ import __version__
 from .tracker import Tracker
 from .commit import Commit
+from .ignore import get_non_ignored
 
 REPO_PATH = os.path.join('./', '.box')
 OBJECTS_PATH = os.path.join(REPO_PATH, 'objects')
@@ -33,6 +34,27 @@ def add(files: list) -> None:
     tracker.track(files)
 
 
+def status() -> None:
+    non_ignored = get_non_ignored()
+    tracked_files = tracker.get_tracked()
+
+    print('\033[1mUncommitted files\033[m')
+    print('Use "commit" argument to commit this files:\033[33m')
+
+    for filepath, info in tracked_files.items():
+        if not info['committed']:
+            print('   ' + filepath.replace('./', ''))
+
+    print('\n\033[37;1mUntracked files\033[m')
+    print('Use "add" argument to track this files:\033[33m')
+
+    for filepath in non_ignored:
+        if filepath not in tracked_files:
+            print('   ' + filepath.replace('./', ''))
+
+    print('\033[m')
+
+
 def main() -> None:
     parser = ArgEasy(
         name='Box',
@@ -42,9 +64,12 @@ def main() -> None:
 
     parser.add_argument('init', 'Init a empty repository', action='store_true')
     parser.add_argument('add', 'Add new files to track list', action='append')
+    parser.add_argument('status', 'View uncommitted and untracked files', action='store_true')
     args = parser.parse()
 
     if args.init:
         init()
     elif args.add:
         add(args.add)
+    elif args.status:
+        status()
