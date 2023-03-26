e43ec17d99d50b512cc7d862a2b283381252758a
Jaedson Silva <contacanal798@gmail.com>
2023-03-26 20:21:55 -0300

    feat: add function to get author info

--- box/_config.py
+++ box/_config.py
@@ -4,3 +4,13 @@ import pathlib
 
 HOME_PATH = pathlib.Path.home()
 BOX_CONFIG_PATH = os.path.join(HOME_PATH, '.box.config.json')
+
+
+def _get_author() -> dict:
+    try:
+        with open(BOX_CONFIG_PATH, 'r') as file:
+            config = json.load(file)
+    except FileNotFoundError:
+        return {}
+    else:
+        return config.get('author')
