d2fdc37f83fc1cee81db7282bb7f28daeadde8f0
Jaedson Silva <contacanal798@gmail.com>
2023-04-13 09:03:18 -0300

    build: add keywords and classifiers

--- setup.py
+++ setup.py
@@ -16,5 +16,15 @@ setup(
         'console_scripts': [
             'box = box.__main__:main'
         ]
-    }
+    },
+    keywords=['version', 'control', 'versioning', 'file'],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Version Control'
+    ],
 )
