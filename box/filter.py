3c9cf57770661f113905d0fb135061e7ee9ed6ec
Jaedson Silva <jaedson.dev@proton.me>
2023-06-09 15:16:54 -0300

    feat: create method to filter by name, email or date

--- box/filter.py
+++ box/filter.py
@@ -21,3 +21,18 @@ class Filter:
 
         filtered_commits = {cid: commits[cid] for cid in filter(fbd, commits)}
         return filtered_commits
+
+    def filter(self, commits: dict, by_name: str = None,
+               by_email: str = None, by_date: str = None) -> dict:
+        filtered_commits = commits
+        
+        if by_name:
+            filtered_commits = self._filter_by_author(filtered_commits, by_name)
+        
+        if by_email:
+            filtered_commits = self._filter_by_email(filtered_commits, by_email)
+
+        if by_date:
+            filtered_commits = self._filter_by_date(filtered_commits, by_date)
+
+        return filtered_commits
