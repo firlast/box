778c92dea3e62a9e2b4638289601741d67e4fcbd
Jaedson Silva <jaedson.dev@proton.me>
2023-06-09 15:07:54 -0300

    feat: remove unnecessary variables

--- box/filter.py
+++ box/filter.py
@@ -1,14 +1,15 @@
+from datetime import datetime
+
+
 class Filter:
     def _filter_by_author(self, commits: dict, author: str):
         filter_by_author = lambda cid: commits[cid]['author'] == author
         filtered_cid = filter(filter_by_author, commits)
-        filtered_commits = {cid: commits[cid] for cid in filtered_cid}
 
-        return filtered_commits
+        return {cid: commits[cid] for cid in filtered_cid}
     
     def _filter_by_email(self, commits: dict, email: str):
         filter_by_author = lambda cid: commits[cid]['author_email'] == email
         filtered_cid = filter(filter_by_author, commits)
-        filtered_commits = {cid: commits[cid] for cid in filtered_cid}
 
-        return filtered_commits
+        return {cid: commits[cid] for cid in filtered_cid}
