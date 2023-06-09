02bf16848a6458acc0780f18c3efef2019a7d6ca
Jaedson Silva <jaedson.dev@proton.me>
2023-06-09 14:53:08 -0300

    feat: create method to filter by author email

--- box/filter.py
+++ box/filter.py
@@ -5,3 +5,10 @@ class Filter:
         filtered_commits = {cid: commits[cid] for cid in filtered_cid}
 
         return filtered_commits
+    
+    def _filter_by_email(self, commits: dict, email: str):
+        filter_by_author = lambda cid: commits[cid]['author_email'] == email
+        filtered_cid = filter(filter_by_author, commits)
+        filtered_commits = {cid: commits[cid] for cid in filtered_cid}
+
+        return filtered_commits
